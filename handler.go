2e4916d3bd3c30e7265a0a52180a1d2a24011e56
Ahmet <detcode@gmail.com>
2017-07-21 16:34:44 +0300

    dns handler

--- handler.go
+++ handler.go
@@ -68,22 +68,85 @@ func (h *DNSHandler) do(Net string, w dns.ResponseWriter, req *dns.Msg) {
 	if q.Qtype == dns.TypeA {
 		// if name is ns
 		var ns Ns
-		err := h.db.QueryRow("SELECT * FROM ns WHERE name = ? LIMIT 1", Q.Qname).Scan(&ns.Id, &ns.Name, &ns.DataV4, &ns.DataV6, &ns.Ttl)
+		err := h.db.QueryRow("SELECT * FROM ns WHERE name = ? LIMIT 1", Q.Qname).Scan(
+			&ns.Id,
+			&ns.Name,
+			&ns.DataV4,
+			&ns.DataV6,
+			&ns.Ttl)
+
 		if err == nil && ns.Id > 0 {
+			// build the reply
 			m := new(dns.Msg)
 			m.SetReply(req)
 
 			rr_header := dns.RR_Header{Name: q.Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: ns.Ttl}
-			a := &dns.A{rr_header, net.ParseIP(ns.DataV4)}
+			a := &dns.A{Hdr: rr_header, A: net.ParseIP(ns.DataV4)}
+
 			m.Answer = append(m.Answer, a)
 
+			// write the reply
 			w.WriteMsg(m)
 			return
 		}
 
-		// if name is record
+		// todo if name is record
 
 	} else if q.Qtype == dns.TypeSOA {
+		// get soa record
+		var soa Soa
+		err := h.db.QueryRow("SELECT * FROM soa WHERE origin = ? LIMIT 1", Q.Qname).Scan(
+			&soa.Id,
+			&soa.UserId,
+			&soa.Origin,
+			&soa.Ns1,
+			&soa.Ns2,
+			&soa.Mbox,
+			&soa.Serial,
+			&soa.Refresh,
+			&soa.Retry,
+			&soa.Expire,
+			&soa.Minimum,
+			&soa.Ttl,
+			&soa.Active,
+		)
 
+		if err == nil && soa.Id > 0 {
+			// get ns records
+			var ns1 Ns
+			err := h.db.QueryRow("SELECT * FROM ns WHERE id = ?", soa.Ns1).Scan(
+				&ns1.Id,
+				&ns1.Name,
+				&ns1.DataV4,
+				&ns1.DataV6,
+				&ns1.Ttl)
+			if err != nil || ns1.Id == 0 {
+				return
+			}
+
+			var ns2 Ns
+			err = h.db.QueryRow("SELECT * FROM ns WHERE id = ?", soa.Ns2).Scan(
+				&ns2.Id,
+				&ns2.Name,
+				&ns2.DataV4,
+				&ns2.DataV6,
+				&ns2.Ttl)
+			if err != nil || ns2.Id == 0 {
+				return
+			}
+
+			// build the reply
+			m:= new(dns.Msg)
+			m.SetReply(req)
+
+			rr_header := dns.RR_Header{ Name: q.Name, Rrtype: dns.TypeSOA, Class: dns.ClassINET, Ttl: soa.Ttl }
+			s := &dns.SOA{ Hdr: rr_header, Ns: ns1.Name, Mbox: soa.Mbox, Serial: soa.Serial, Refresh: soa.Refresh, Retry: soa.Retry, Expire: soa.Expire, Minttl: soa.Minimum}
+
+			m.Answer = append(m.Answer, s)
+
+			// write the reply
+			w.WriteMsg(m)
+			return
+		}
 	}
 }
--- model.go
+++ model.go
@@ -1,9 +1,25 @@
 package main
 
 type Ns struct {
-	Id		int
+	Id		uint32
 	Name		string
 	DataV4		string
 	DataV6		string
 	Ttl		uint32
+}
+
+type Soa struct {
+	Id		uint32
+	UserId		uint32
+	Origin		string
+	Ns1		uint32
+	Ns2		uint32
+	Mbox		string
+	Serial		uint32
+	Refresh		uint32
+	Retry		uint32
+	Expire		uint32
+	Minimum		uint32
+	Ttl		uint32
+	Active		string
 }
